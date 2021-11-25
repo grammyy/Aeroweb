7a99940480841d730e6c74456c5c5068fcb960ee
Bartender <bartender.winery@gmail.com>
2021-11-24 19:40:39 -0500

    Updating CET to dynamically change text

---
+++ engine/CET.js
@@ -0,0 +1,22 @@
+fetch('./package.json')
+  .then(response => response.json())
+  .then(jsonResponse => console.log(jsonResponse))     
+var printText = "░█████╗░███████╗██████╗░░█████╗░░██╗░░░░░░░██╗███████╗██████╗░/n"+""+
+"██╔══██╗██╔════╝██╔══██╗██╔══██╗░██║░░██╗░░██║██╔════╝██╔══██╗/n"+
+"███████║█████╗░░██████╔╝██║░░██║░╚██╗████╗██╔╝█████╗░░██████╦╝/n"+
+"██╔══██║██╔══╝░░██╔══██╗██║░░██║░░████╔═████║░██╔══╝░░██╔══██╗/n"+
+"██║░░██║███████╗██║░░██║╚█████╔╝░░╚██╔╝░╚██╔╝░███████╗██████╦╝/n"+
+"╚═╝░░╚═╝╚══════╝╚═╝░░╚═╝░╚════╝░░░░╚═╝░░░╚═╝░░╚══════╝╚═════╝░/n"+
+"══════════════════════════════════════════════════════════════/n"+
+"Aeroweb version:"+version+"/n"
+
+var contentArray = printText.split("/n");
+$.each(contentArray, function(index, newLine) {
+  $('.text').append('<span style="display:block;" id="'+index+'"></span>');
+
+  var lineID = index; var self = $(this);
+    setTimeout(function () {
+      $.each(self, function(index, chunk){ setTimeout(function () {
+            $('#'+lineID).append("<span>"+chunk+"</span>");
+            $('body, html').scrollTop($(document).height());
+        }, index*5);}); }, index*100); });
