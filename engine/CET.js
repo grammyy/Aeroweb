87b8673c35b4581c1a236f1793beab471da47358
Bartender <bartender.winery@gmail.com>
2021-11-24 20:06:05 -0500

    Console emulator complete for the time being

--- engine/CET.js
+++ engine/CET.js
@@ -1,7 +1,5 @@
-fetch('./package.json')
-  .then(package => package.json())
-  .then(response => gotversion = response.version)   
-if(gotversion===null){gotversion=null}
+var version = 1.1
+
 var printText = "░█████╗░███████╗██████╗░░█████╗░░██╗░░░░░░░██╗███████╗██████╗░/n"+""+
 "██╔══██╗██╔════╝██╔══██╗██╔══██╗░██║░░██╗░░██║██╔════╝██╔══██╗/n"+
 "███████║█████╗░░██████╔╝██║░░██║░╚██╗████╗██╔╝█████╗░░██████╦╝/n"+
@@ -9,10 +7,19 @@ var printText = "░█████╗░███████╗█████
 "██║░░██║███████╗██║░░██║╚█████╔╝░░╚██╔╝░╚██╔╝░███████╗██████╦╝/n"+
 "╚═╝░░╚═╝╚══════╝╚═╝░░╚═╝░╚════╝░░░░╚═╝░░░╚═╝░░╚══════╝╚═════╝░/n"+
 "══════════════════════════════════════════════════════════════/n"+
-"Aeroweb version:"+gotversion+"/n"
+"Aeroweb version: "+version+"/n /nFeatures coming soon;/n"+
+"JSON support for dyanmic text;/n"+
+"Electron build of website;/n"+
+"Multiverse of all examples and code;/n /n"+
+"Resources;/n"+
+"Debug; Null/n"+
+"Main; Null/n"+
+"Source; Null/n"+
+"Games; Null/n /n"+
+"Console v2 coming soon!"
 
 var contentArray = printText.split("/n");
-$.each(contentArray, function(index, newLine) {
+$.each(contentArray, function(index) {
   $('.text').append('<span style="display:block;" id="'+index+'"></span>');
 
   var lineID = index; var self = $(this);
