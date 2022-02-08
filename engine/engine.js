9de96ffddd36bc359101e9d2da5c283fea326b89
Bartender <bartender.winery@gmail.com>
2022-02-07 21:33:08 -0500

    I'm stupid, I fixed the console

--- engine/engine.js
+++ engine/engine.js
@@ -1,39 +1,20 @@
  
+var console = document.getElementById("console");
 setInterval(function() { clock();}, 500);
-
-const WelMSG = ""+
-"░█████╗░███████╗██████╗░░█████╗░░██╗░░░░░░░██╗███████╗██████╗░/n"+
-"██╔══██╗██╔════╝██╔══██╗██╔══██╗░██║░░██╗░░██║██╔════╝██╔══██╗/n"+
-"███████║█████╗░░██████╔╝██║░░██║░╚██╗████╗██╔╝█████╗░░██████╦╝/n"+
-"██╔══██║██╔══╝░░██╔══██╗██║░░██║░░████╔═████║░██╔══╝░░██╔══██╗/n"+
-"██║░░██║███████╗██║░░██║╚█████╔╝░░╚██╔╝░╚██╔╝░███████╗██████╦╝/n"+
-"╚═╝░░╚═╝╚══════╝╚═╝░░╚═╝░╚════╝░░░░╚═╝░░░╚═╝░░╚══════╝╚═════╝░/n"+
-"══════════════════════════════════════════════════════════════/n"+
-"IMPORTANT! THIS WEBSITE IS NOT FINISHED AND PROBABLY WON'T BE FOR A LONG TIME./n"+
-"IF YOU'RE SEEKING COMMUNICATION, DM THE DISCORD ACCOUNT: Bartender#0531"
+const WelMSG = [
+    "░█████╗░███████╗██████╗░░█████╗░░██╗░░░░░░░██╗███████╗██████╗░",
+    "██╔══██╗██╔════╝██╔══██╗██╔══██╗░██║░░██╗░░██║██╔════╝██╔══██╗",
+    "███████║█████╗░░██████╔╝██║░░██║░╚██╗████╗██╔╝█████╗░░██████╦╝",
+    "██╔══██║██╔══╝░░██╔══██╗██║░░██║░░████╔═████║░██╔══╝░░██╔══██╗",
+    "██║░░██║███████╗██║░░██║╚█████╔╝░░╚██╔╝░╚██╔╝░███████╗██████╦╝",
+    "╚═╝░░╚═╝╚══════╝╚═╝░░╚═╝░╚════╝░░░░╚═╝░░░╚═╝░░╚══════╝╚═════╝░",
+    "══════════════════════════════════════════════════════════════",
+    "IMPORTANT! THIS WEBSITE IS NOT FINISHED AND PROBABLY WON'T BE FOR A LONG TIME.",
+    "IF YOU'RE SEEKING COMMUNICATION, DM THE DISCORD ACCOUNT: Bartender#0531"]
 //const songs = require('../packages/songs.json');
 function clock() { var cl = new Date(); document.getElementById("clock").innerHTML = cl.toLocaleTimeString();}
-function rcon(type, array) { 
-    switch(type){
-        case "console":
-            const package = {log: "Logged message from console; formatted as const;"}
-            console.info(package.log); console.log(array);
-            break
-        
-    }
-}
-function con(request){
-    var array = request.split("/n");
-    rcon("console", array);
-    $.each(array, function(index) {
-        $('.text').append('<span style="display:block;" id="'+index+'"></span>');
-        var lineID = index; var self = $(this);
-          setTimeout(function () {
-            $.each(self, function(index, chunk){ setTimeout(function () {
-                  $('#'+lineID).append("<span>"+chunk+"</span>");
-                  $('body, html').scrollTop($(document).height());
-              }, index*5);}); }, index*100);});
-}
+var con = function(request){
+        request.forEach(element => console.insertAdjacentHTML("beforeend","<p>"+element+"</p>"))}
 function init(){
     con(WelMSG);
 }
