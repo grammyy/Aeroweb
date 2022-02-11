7b4bb951b0eaf98588127163fb1469c448a85b7a
Bartender <bartender.winery@gmail.com>
2022-02-11 09:50:43 -0500

    Removed top bar in cc and added fullscreen

--- engine/engine.js
+++ engine/engine.js
@@ -15,82 +15,81 @@ const WelMSG = [
     "══════════════════════════════════════════════════════════════",
     "If the layout of this website seems wrong, your HTML scale may be too low",
     "The problem is fixable, it just would take a while. Expect a proper layout update soon."]
-const Changelog = [
-    "==---------------------------------------------------------------------------------------------==",
-    "+Added Cookie Clicker 2.031 (Latest version)"
-]
-//const songs = require('../packages/songs.json');
-function clock() { var cl = new Date(); document.getElementById("clock").innerHTML = cl.toLocaleTimeString();}
-var con = function(func,request){
-    switch(func){
-        case "log":
-            request.forEach(element => console.insertAdjacentHTML("beforeend","<p>"+element+"</p>"))
-            break;
-        case "screen":
-            if (fullscreen==0){ //needs improving lol
-                console.style.height = "100%";
-                console.style.width = "100%";
-                console.style.position = "initial";
-                console.style.margin = "0";
-                document.getElementById("folder").style.visibility = "hidden";
-                document.getElementById("multiverse").style.visibility = "hidden";
-                controller.style.visibility = "hidden";
-                control.style.visibility = "visible";
-                control.style.position = "fixed";
-                control.style.top = "0";
-                fullscreen=1
-            } else {
-                console.style.height = "66%";
-                console.style.width = "73%";
-                console.style.position = "absolute";
-                console.style.margin = "0.5%";
-                document.getElementById("folder").style.visibility = "visible";
-                document.getElementById("multiverse").style.visibility = "visible";
-                control.style.visibility = "visible";
-                control.style.position = "absolute";
-                controller.style.visibility = "visible";
-                control.style.top = "6%";
-                fullscreen = 0 
-            }
-            break;
-            case "fullscreen":
-                if (fullscreener==0){ //needs improving lol
-                    console.style.height = "100%";
-                    console.style.width = "100%";
-                    console.style.position = "initial";
-                    console.style.margin = "0";
-                    document.getElementById("folder").style.visibility = "hidden";
-                    document.getElementById("multiverse").style.visibility = "hidden";
-                    controller.style.visibility = "visible";
-                    controller.style.position = "fixed";
-                    control.style.visibility = "hidden";
-                    universal.style.height = "100%";
-                    universal.style.width = "100%";
-                    universal.style.border = "unset";
-                    fullscreener = 1
-                } else {
-                    console.style.height = "66%";
-                    console.style.width = "73%";
-                    console.style.position = "absolute";
-                    console.style.margin = "0.5%";
-                    document.getElementById("folder").style.visibility = "visible";
-                    document.getElementById("multiverse").style.visibility = "visible";
-                    controller.style.visibility = "visible";
-                    controller.style.position = "absolute";
-                    control.style.visibility = "visible";
-                    universal.style.height = "80%";
-                    universal.style.width = "90%";
-                    universal.style.border = "1px dotted red";
-                    fullscreener = 0 
-                }
+    const Changelog = [
+        "==---------------------------------------------------------------------------------------------==",
+        "+Added Cookie Clicker 2.031 (Latest version)"
+    ]
+    //const songs = require('../packages/songs.json');
+    function clock() { var cl = new Date(); document.getElementById("clock").innerHTML = cl.toLocaleTimeString();}
+    var con = function(func,request){
+        switch(func){
+            case "log":
+                request.forEach(element => console.insertAdjacentHTML("beforeend","<p>"+element+"</p>"))
                 break;
-        case "clear":
-            console.innerHTML="";
-            console.style.opacity = "60%";
-            break;
+                case "screen":
+                    if (fullscreen==0){ //needs improving lol
+                        console.style.height = "100%";
+                        console.style.width = "100%";
+                        console.style.position = "initial";
+                        console.style.margin = "0";
+                        document.getElementById("folder").style.visibility = "hidden";
+                        document.getElementById("multiverse").style.visibility = "hidden";
+                        controller.style.visibility = "hidden";
+                        control.style.visibility = "visible";
+                        control.style.position = "fixed";
+                        control.style.top = "0";
+                        fullscreen=1
+                    } else {
+                        console.style.height = "66%";
+                        console.style.width = "73%";
+                        console.style.position = "absolute";
+                        console.style.margin = "0.5%";
+                        document.getElementById("folder").style.visibility = "visible";
+                        document.getElementById("multiverse").style.visibility = "visible";
+                        control.style.visibility = "visible";
+                        control.style.position = "absolute";
+                        controller.style.visibility = "visible";
+                        control.style.top = "6%";
+                        fullscreen = 0 
+                    }
+                    break;
+                    case "fullscreen":
+                        if (fullscreener==0){ //needs improving lol
+                            console.style.height = "100%";
+                            console.style.width = "100%";
+                            console.style.position = "initial";
+                            console.style.margin = "0";
+                            document.getElementById("folder").style.visibility = "hidden";
+                            document.getElementById("multiverse").style.visibility = "hidden";
+                            controller.style.visibility = "visible";
+                            controller.style.position = "fixed";
+                            control.style.visibility = "hidden";
+                            universal.style.height = "100%";
+                            universal.style.width = "100%";
+                            universal.style.border = "unset";
+                            fullscreener = 1
+                        } else {
+                            console.style.height = "66%";
+                            console.style.width = "73%";
+                            console.style.position = "absolute";
+                            console.style.margin = "0.5%";
+                            document.getElementById("folder").style.visibility = "visible";
+                            document.getElementById("multiverse").style.visibility = "visible";
+                            controller.style.visibility = "visible";
+                            controller.style.position = "absolute";
+                            control.style.visibility = "visible";
+                            universal.style.height = "80%";
+                            universal.style.width = "90%";
+                            universal.style.border = "1px dotted red";
+                            fullscreener = 0 
+                        }
+                        break;
+                        case "clear":
+                            console.innerHTML="";
+                            console.style.opacity = "60%";
+                            break;
     }
 }
-    //
 function init(){
     con("log",WelMSG);
     con("log",Changelog)
--- multiverse/main.js
+++ multiverse/main.js
@@ -1222,10 +1222,6 @@ Game.Launch=function()
 		if (Game.beta) Game.SaveTo='CookieClickerGameBeta';
 		l('versionNumber').innerHTML='v. '+Game.version+'<div id="httpsSwitch" style="cursor:pointer;display:inline-block;background:url(img/'+(Game.https?'lockOn':'lockOff')+'.png);width:16px;height:16px;position:relative;top:4px;left:0px;margin:0px -2px;"></div>'+(Game.beta?' <span style="color:#ff0;">beta</span>':'');
 		
-		if (Game.beta) {var me=l('linkVersionBeta');me.parentNode.removeChild(me);}
-		else if (Game.version==1.0466) {var me=l('linkVersionOld');me.parentNode.removeChild(me);}
-		else {var me=l('linkVersionLive');me.parentNode.removeChild(me);}
-
 		//l('links').innerHTML=(Game.beta?'<a href="../" target="blank">Live version</a> | ':'<a href="beta" target="blank">Try the beta!</a> | ')+'<a href="http://orteil.dashnet.org/experiments/cookie/" target="blank">Classic</a>';
 		//l('links').innerHTML='<a href="http://orteil.dashnet.org/experiments/cookie/" target="blank">Cookie Clicker Classic</a>';
 		
@@ -1868,12 +1864,7 @@ Game.Launch=function()
 		
 		Game.grandmaNames=['Granny','Gusher','Ethel','Edna','Doris','Maud','Hilda','Gladys','Michelle','Michele','Phyllis','Millicent','Muriel','Myrtle','Mildred','Mavis','Helen','Gloria','Sheila','Betty','Gertrude','Agatha','Beryl','Agnes','Pearl','Precious','Ruby','Vera','Bonnie','Ada','Bunny','Cookie','Darling','Gaga','GamGam','Memaw','Mimsy','Peanut','Nana','Nan','Tootsie','Warty','Stinky','Heinous'];
 		Game.customGrandmaNames=[];
-		Game.heralds=0;
-		
-		Game.GrabData=function()
-		{
-			ajax('/patreon/grab.php',Game.GrabDataResponse);
-		}
+
 		Game.GrabDataResponse=function(response)
 		{
 			/*
@@ -1907,46 +1898,6 @@ Game.Launch=function()
 			if (location.protocol=='https:') location.href='http:'+window.location.href.substring(window.location.protocol.length);
 			else if (location.protocol=='http:') location.href='https:'+window.location.href.substring(window.location.protocol.length);
 		});
-		
-		Game.attachTooltip(l('topbarOrteil'),'<div style="padding:8px;width:250px;text-align:center;">Back to Orteil\'s subdomain!<br>Lots of other games in there!</div>','this');
-		Game.attachTooltip(l('topbarDashnet'),'<div style="padding:8px;width:250px;text-align:center;">Back to our homepage!</div>','this');
-		Game.attachTooltip(l('topbarTwitter'),'<div style="padding:8px;width:250px;text-align:center;">Orteil\'s twitter, which frequently features game updates.</div>','this');
-		Game.attachTooltip(l('topbarTumblr'),'<div style="padding:8px;width:250px;text-align:center;">Orteil\'s tumblr, which frequently features game updates.</div>','this');
-		Game.attachTooltip(l('topbarDiscord'),'<div style="padding:8px;width:250px;text-align:center;">Our official discord server.<br>You can share tips and questions about Cookie Clicker and all our other games!</div>','this');
-		Game.attachTooltip(l('topbarPatreon'),'<div style="padding:8px;width:250px;text-align:center;">Support us on Patreon and help us keep updating Cookie Clicker!<br>There\'s neat rewards for patrons too!</div>','this');
-		Game.attachTooltip(l('topbarMerch'),'<div style="padding:8px;width:250px;text-align:center;">Cookie Clicker shirts, hoodies and stickers!</div>','this');
-		Game.attachTooltip(l('topbarMobileCC'),'<div style="padding:8px;width:250px;text-align:center;">Play Cookie Clicker on your phone!<br>(Android only; iOS version will be released later)</div>','this');
-		Game.attachTooltip(l('topbarSteamCC'),'<div style="padding:8px;width:250px;text-align:center;">Get Cookie Clicker on Steam!</div>','this');
-		Game.attachTooltip(l('topbarRandomgen'),'<div style="padding:8px;width:250px;text-align:center;">A thing we made that lets you write random generators.</div>','this');
-		Game.attachTooltip(l('topbarIGM'),'<div style="padding:8px;width:250px;text-align:center;">A thing we made that lets you create your own idle games using a simple scripting language.</div>','this');
-		
-		Game.attachTooltip(l('heralds'),function(){
-			var str='';
-			
-			if (!Game.externalDataLoaded) str+='Heralds couldn\'t be loaded. There may be an issue with our servers, or you are playing the game locally.';
-			else
-			{
-				if (Game.heralds==0) str+='There are no heralds at the moment. Please consider <b style="color:#bc3aff;">donating to our Patreon</b>!';
-				else
-				{
-					str+=(Game.heralds==1?'<b style="color:#bc3aff;text-shadow:0px 1px 0px #6d0096;">1 herald</b> is':'<b style="color:#fff;text-shadow:0px 1px 0px #6d0096,0px 0px 6px #bc3aff;">'+Game.heralds+' heralds</b> are')+' selflessly inspiring a boost in production for everyone, resulting in<br><b style="color:#cdaa89;text-shadow:0px 1px 0px #7c4532,0px 0px 6px #7c4532;"><div style="width:16px;height:16px;display:inline-block;vertical-align:middle;background:url(img/money.png);"></div> +'+Game.heralds+'% cookies per second</b>.';
-					str+='<div class="line"></div>';
-					if (Game.ascensionMode==1) str+='You are in a <b>Born again</b> run, and are not currently benefiting from heralds.';
-					else if (Game.Has('Heralds')) str+='You own the <b>Heralds</b> upgrade, and therefore benefit from the production boost.';
-					else str+='To benefit from the herald bonus, you need a special upgrade you do not yet own. You will permanently unlock it later in the game.';
-				}
-			}
-			str+='<div class="line"></div><span style="font-size:90%;opacity:0.6;"><b>Heralds</b> are people who have donated to our highest Patreon tier, and are limited to 100.<br>Each herald gives everyone +1% CpS.<br>Heralds benefit everyone playing the game, regardless of whether you donated.</span>';
-			
-			str+='<div style="width:31px;height:39px;background:url(img/heraldFlag.png);position:absolute;top:0px;left:8px;"></div><div style="width:31px;height:39px;background:url(img/heraldFlag.png);position:absolute;top:0px;right:8px;"></div>';
-			
-			return '<div style="padding:8px;width:300px;text-align:center;" class="prompt"><h3>Heralds</h3><div class="block">'+str+'</div></div>';
-		},'this');
-		l('heraldsAmount').textContent='?';
-		l('heralds').style.display='inline-block';
-		
-		Game.GrabData();
-		
 		
 		Game.useLocalStorage=1;
 		Game.localStorageGet=function(key)
