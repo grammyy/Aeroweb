f6598faa4c7ff95264f4ff37736dab93a7cdf129
Bartender <bartender.winery@gmail.com>
2022-03-06 14:04:51 -0500

    upload

--- modules/drag.js
+++ modules/drag.js
@@ -1,40 +1,34 @@
 function dragElement(elmnt) {
-  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  const position =[0,0,0,0]
   if (document.getElementById(elmnt.id+"toolbar")) {
     // if present, the header is where you move the DIV from:
     document.getElementById(elmnt.id+"toolbar").onmousedown = dragMouseDown;
   } else {
     // otherwise, move the DIV from anywhere inside the DIV:
     elmnt.onmousedown = dragMouseDown;
-  }
-
+  } 
   function dragMouseDown(e) {
     e = e || window.event;
     e.preventDefault();
     // get the mouse cursor position at startup:
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    document.onmouseup = closeDragElement;
+    position[2] = e.clientX;
+    position[3] = e.clientY;
+    document.onmouseup = function(){
+        // stop moving when mouse button is released:
+        document.onmouseup = null;
+        document.onmousemove = null;
+    };
     // call a function whenever the cursor moves:
-    document.onmousemove = elementDrag;
-  }
-
-  function elementDrag(e) {
+document.onmousemove = function(e){      
     e = e || window.event;
     e.preventDefault();
     // calculate the new cursor position:
-    pos1 = pos3 - e.clientX;
-    pos2 = pos4 - e.clientY;
-    pos3 = e.clientX;
-    pos4 = e.clientY;
+    position[0] = position[2] - e.clientX;
+    position[1] = position[3] - e.clientY;
+    position[2] = e.clientX;
+    position[3] = e.clientY;
     // set the element's new position:
-    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-  }
-
-  function closeDragElement() {
-    // stop moving when mouse button is released:
-    document.onmouseup = null;
-    document.onmousemove = null;
-  }
-} //example from w3school, (literally only example that did what i wanted it to), creating custom version soon
+    elmnt.style.top = (elmnt.offsetTop - position[1]) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - position[0]) + "px";
+    };
+}}
