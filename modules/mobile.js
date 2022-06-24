267a6bd7ce579db96745188f718b51d58a807db0
Bartender <66457110+BartenderWinery@users.noreply.github.com>
2022-06-23 23:28:36 -0400

    fixed dumb typos

--- modules/mobile.js
+++ modules/mobile.js
@@ -3,13 +3,13 @@ setInterval(function(){
         webpage.children[0].style.display=webpage.children[1].style.display="none"
         webpage.children[2].style.marginLeft=webpage.children[3].style.marginLeft="0px"
         webpage.children[2].style.marginRight=webpage.children[3].style.marginRight="0px"
-        if(webpage.children[4].tagName!="IFRAME")webpage.children[3].insertAdjacentHTML("afterend","<iframe src='./programs/mobile.html' style='margin-bottom:31%;bottom:0;height:10px'></iframe>")
+        try{if(webpage.children[4].tagName!="IFRAME")webpage.children[3].insertAdjacentHTML("afterend","<iframe src='./programs/mobile.html' style='margin-bottom:31%;bottom:0;height:10px'></iframe>")}catch(e){}
         webpage.children[3].className="subwindow mobile"
         webpage.children[3].children["toolkit"].className="mobile"
     }else{
         webpage.children[0].style.display=webpage.children[1].style.display="block"
         webpage.children[2].style.marginLeft=webpage.children[3].style.marginLeft="11%"
         webpage.children[2].style.marginRight=webpage.children[3].style.marginRight="16%"
-        if(webpage.children[4].tagName="IFRAME")webpage.children[4].remove()
+        try{if(webpage.children[4].tagName="IFRAME")webpage.children[4].remove()}catch(e){}
         webpage.children[3].className="subwindow"
         webpage.children[3].children["toolkit"].className=""}},250)
