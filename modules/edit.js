4d8b17cdbc2a7ade4167d36809356987dae1e387
Bartender <66457110+BartenderWinery@users.noreply.github.com>
2022-06-24 23:08:02 -0400

    editor

---
+++ modules/edit.js
@@ -0,0 +1,13 @@
+window.onload=function(){
+    try{edit}catch(e){
+        console.error("You are missing required App configurations for editing")}}
+var controls={
+    save:function(t){
+        switch(t){
+            case "url":
+                break
+            case "cookie":
+                break
+            case "download":
+                break}},
+    load:function(d){}}
